12f90e277e7962b344681d35749890b1a54b8458
ANOSbot12 <145455552+ANOSbot12@users.noreply.github.com>
2023-10-19 01:32:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
+global.namabot = "Rangga BOT"
+global.namaowner = "Rangga"
 global.packname = namabot
 global.author = namaowner
 global.myweb = "https://s.id/zeeoneofc"
@@ -19,8 +19,8 @@ global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
 global.owner = ['62887435047326']
-global.pairingNumber = "" //pairing number to install bot 
-global.sessionName = 'session'
+global.pairingNumber = "6281717250393" //pairing number to install bot 
+global.sessionName = 'sessionff'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
 global.grup_only = false //bot hanya bisa di gunakan dalam grup jika true
